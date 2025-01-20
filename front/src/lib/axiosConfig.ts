df5b24044d7da1e74a08a3a582e14abfae3e3449
Zaharov Andrey <zaharov.andrey161@gmail.com>
2025-01-20 22:02:22 +0300

    Fixed axios config for https.

--- front/src/lib/axiosConfig.ts
+++ front/src/lib/axiosConfig.ts
@@ -1,5 +1,6 @@
 import axios from "axios"
 
+export const backendHost = ((import.meta.env.VITE_DEBUG == "1") ? "http://" : "https://") + (import.meta.env.VITE_BACKEND_IP ?? "localhost:8000") + '/'
 const Axios = axios.create({
     xsrfCookieName: 'csrftoken',
 	xsrfHeaderName: 'X-CSRFToken',
