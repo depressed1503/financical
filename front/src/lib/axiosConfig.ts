e59831e7cf76a372b1fe7455264a3806ab985556
Zaharov Andrey <zaharov.andrey161@gmail.com>
2025-01-20 22:07:32 +0300

    fix.

--- front/src/lib/axiosConfig.ts
+++ front/src/lib/axiosConfig.ts
@@ -1,6 +1,7 @@
 import axios from "axios"
 
 export const backendHost = ((import.meta.env.VITE_DEBUG == "1") ? "http://" : "https://") + (import.meta.env.VITE_BACKEND_IP ?? "localhost:8000") + '/'
+console.log(import.meta.env.VITE_DEBUG, backendHost)
 const Axios = axios.create({
     xsrfCookieName: 'csrftoken',
 	xsrfHeaderName: 'X-CSRFToken',
