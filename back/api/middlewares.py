cf771f63d7e8877c9b529f78337d8cd1a9e883d0
Zaharov Andrey <zaharov.andrey161@gmail.com>
2025-01-25 14:37:35 +0300

    fix middleware check.

--- back/api/middlewares.py
+++ back/api/middlewares.py
@@ -2,16 +2,6 @@ from django.middleware.csrf import CsrfViewMiddleware
 
 
 class CSRFDebugMiddleware(CsrfViewMiddleware):
-    def __init__(self, get_response):
-        self.get_response = get_response
-
-    def __call__(self, request):
-        response = self.get_response(request)
-        if request.method == "POST":
-            print(f"CSRF Cookie: {request.META.get('CSRF_COOKIE')}")
-            print(f"X-CSRFToken Header: {request.headers.get('X-CSRFToken')}")
-        return response
-
     def process_view(self, request, callback, callback_args, callback_kwargs):
         print("\n[CSRF DEBUG]")
         print("Cookies:", request.COOKIES)
