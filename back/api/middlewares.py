7ec934c205c99c3f31bd1bca3b76ce2e9417519a
Zaharov Andrey <zaharov.andrey161@gmail.com>
2025-01-25 14:54:11 +0300

    fix.

--- back/api/middlewares.py
+++ back/api/middlewares.py
@@ -7,5 +7,6 @@ class CSRFDebugMiddleware(CsrfViewMiddleware):
         print("Cookies:", request.COOKIES)
         print("Meta CSRF:", request.META.get("CSRF_COOKIE"))
         print("Headers CSRF:", request.headers.get("X-Csrftoken"))
+        print("Headers", dict(request.headers))
         print("POST CSRF:", request.POST.get('csrfmiddlewaretoken', ''))
         return super().process_view(request, callback, callback_args, callback_kwargs)
