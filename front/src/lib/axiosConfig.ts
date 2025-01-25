4081fae5531262d9821004b16ba1937d182bfb57
Zaharov Andrey <zaharov.andrey161@gmail.com>
2025-01-25 15:46:41 +0300

    Updated axios and nginx config.

--- front/src/lib/axiosConfig.ts
+++ front/src/lib/axiosConfig.ts
@@ -1,5 +1,4 @@
 import axios from "axios"	
-import Cookies from "js-cookie"
 
 export const backendHost = ((import.meta.env.VITE_DEBUG == "1") ? "http://" : "https://") + (import.meta.env.VITE_BACKEND_IP ?? "127.0.0.1:8000") + '/'
 const Axios = axios.create({
@@ -14,19 +13,4 @@ const Axios = axios.create({
 	},
 })
 
-Axios.interceptors.request.use(config => {
-    // Правильный способ получения куки
-    const csrfToken = Cookies.get("csrftoken");
-    
-    // Для дебага - проверим значение токена
-    console.log("[AXIOS] Current CSRF Token:", csrfToken);
-    
-    if (csrfToken) {
-        config.headers["X-CSRFToken"] = csrfToken;
-    } else {
-        console.warn("CSRF token not found in cookies!");
-    }
-    
-    return config;
-});
 export default Axios
